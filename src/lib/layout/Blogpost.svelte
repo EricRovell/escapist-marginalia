a8b6fbe45cd1fbf4bd273e284424df542113cbef
Eric Rovell <ericrovell@gmail.com>
2021-08-27 18:27:54 +0300

    create project layouts

---
+++ src/lib/layout/Blogpost.svelte
@@ -0,0 +1,49 @@
+<script>
+	/* eslint-disable @typescript-eslint/restrict-template-expressions */
+	import { theme } from "@stores";
+
+	export let title;
+
+	$: highlight = $theme === "dark" ? "dark" : "light";
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<link rel="stylesheet" href={`/styles/prism-${highlight}.css`} />
+</svelte:head>
+
+<header>
+	<h1>{title}</h1>
+</header>
+<article>
+	<slot />
+</article>
+
+<style>
+	header {
+		display: grid;
+		gap: 0.75em;
+		width: 100%;
+		margin-bottom: 10vh;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	article {
+		display: grid;
+		width: min(65ch, 100%);
+		justify-self: center;
+		gap: 0.75em;
+	}
+
+	article :global(h2),
+	article :global(h3),
+	article :global(h4),
+	article :global(h5),
+	article :global(h6) {
+		margin-top: var(--spacing-6);
+	}
+</style>
---
+++ src/lib/layout/Root.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+  import { pathRoot, pathBlog, pathGallery, pathProjects, pathAbout } from "@paths";
+  import { Navigation, Footer, Icon, ThemeSelect, Masthead } from "../components";
+  import { iconTelescope } from "@components/icons/default";
+
+  import "@styles/main.css";
+  import "@styles/normalize.css";
+  import "@styles/theme-dark.css";
+  import "@styles/theme-light.css";
+  import "@styles/typography.css";
+</script>
+
+<div id="app">
+  <Masthead>
+    <a class="logo" href={pathRoot} slot="logo">
+      <Icon path={iconTelescope} />
+      <span>eric/rovell</span>
+    </a>
+    <svelte:fragment slot="navigation">
+      <Navigation.Container>
+        <Navigation.Item href={pathRoot}>
+          Home
+        </Navigation.Item>
+        <Navigation.Item href={pathBlog} pattern={/\/blog|\/content/}>
+          Blog
+        </Navigation.Item>
+        <Navigation.Item href={pathGallery} pattern={/\/gallery/}>
+          Gallery
+        </Navigation.Item>
+        <Navigation.Item href={pathProjects}>
+          Projects
+        </Navigation.Item>
+        <Navigation.Item href={pathAbout}>
+          About
+        </Navigation.Item>
+      </Navigation.Container>
+    </svelte:fragment>
+    <svelte:fragment slot="controls">
+      <ThemeSelect size="30px" />
+    </svelte:fragment>
+  </Masthead>
+	<section label="content" class="content">
+		<slot />
+	</section>
+	<Footer>
+		<section class="copyright">
+      © {new Date().getFullYear()} eric/rovell, v0.1.0 | CC BY-NC-SA 4.0 Licence.
+    </section>
+	</Footer>
+</div>
+
+<style>
+  :global(body) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+    line-height: 1.0;
+
+    position: relative;
+  }
+  
+  #app {
+    --max-width: 1440px;
+    width: 100%;
+    
+    font-family: var(--font-main);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1 1 0%;
+  }
+
+  .content {
+    display: grid;
+    grid-auto-rows: min-content;
+    place-items: start start;
+    width: min(100%, var(--max-width));
+    min-height: calc(100vh - 50px);
+    margin: 15vh 0;
+  }
+
+  .copyright {
+    justify-self: start;
+  }
+
+  @media (max-width: 540px) {
+    .copyright {
+      justify-self: center;
+    }
+  }
+
+  /* let bottom menu be full width */
+  @media screen and (max-width: 640px) {
+    #app {
+      padding: 0;
+    }
+    
+    .content {
+      padding: 0 1em;
+    }
+  }
+</style>
