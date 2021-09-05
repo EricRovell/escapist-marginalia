182ca4e33563aaffa7e826b3c9c89270b3f1c9fe
Eric Rovell <ericrovell@gmail.com>
2021-09-05 15:37:19 +0300

    improved blogpost headings styles

--- src/lib/layout/Blogpost.svelte
+++ src/lib/layout/Blogpost.svelte
@@ -39,11 +39,15 @@
 		gap: 0.75em;
 	}
 
-	article :global(h2),
-	article :global(h3),
-	article :global(h4),
-	article :global(h5),
-	article :global(h6) {
-		margin-top: var(--spacing-6);
+	article :global(:is(h1, h2, h3, h4, h5, h6)) {
+		--border-size: 2px;
+
+		margin-top: calc(8 * var(--spacing-em-base));
+		padding-bottom: var(--spacing-2);
+		border-bottom: solid var(--border-size) hsl(var(--gray-h) var(--gray-s-600) var(--gray-l-600) / 0.5);
+
+		text-decoration: underline;
+		text-decoration-color: var(--color-gray-800);
+		text-underline-offset: calc(var(--spacing-2) + var(--border-size));
 	}
 </style>
