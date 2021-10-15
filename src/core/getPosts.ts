7bd7b08ed1532b62d5d8f0061468a0d2b798b355
Eric Rovell <ericrovell@gmail.com>
2021-10-16 00:03:37 +0300

    fix: type cast

--- src/core/getPosts.ts
+++ src/core/getPosts.ts
@@ -19,7 +19,7 @@ export async function getPosts(): Promise<Post[]> {
 
 		posts.push({
 			title,
-			date: new Date(created),
+			date: new Date(created as Date),
 			series,
 			slug: basename(filename, ".svx")
 		});
