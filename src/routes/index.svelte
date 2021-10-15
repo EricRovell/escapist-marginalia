4529ac43a76f31f301744dc6e77b3a8e88944e60
Eric Rovell <ericrovell@gmail.com>
2021-10-15 20:46:11 +0300

    feat(routes): move `timeline` to `index` page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,39 @@
-<script>
-	import { WorkInProgress } from "@components";
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ fetch }) {
+		const res = await fetch("/api/blog/posts.json");
+
+		if (res.ok) {
+			return {
+				props: {
+					posts: await res.json()
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error("Could not load URL")
+		};
+	}
 </script>
 
-<WorkInProgress />
+<script>
+	import { pathBlogpost } from "@paths";
+	import { Card, Timeline } from "@components";
+
+	export let posts = [];
+</script>
+
+<Timeline.Container>
+	{#each posts as { title, date, slug }}
+		<Timeline.Item {date}>
+			<Card
+				{title}
+				href={pathBlogpost(slug)}
+			/>
+		</Timeline.Item>
+	{/each}
+</Timeline.Container>
