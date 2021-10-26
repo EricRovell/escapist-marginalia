58f84d7abf0dae091b5069642d342dd79065ebd7
Eric Rovell <ericrovell@gmail.com>
2021-10-26 15:05:05 +0300

    ecnhancement(types): typing `Project` and github api response

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,22 @@
 export type Variant = "neutral" | "primary" | "success" | "danger" | "warning" | "info" | "link";
 
-export interface Project {
+/**
+ * Defines a used `repo` object subset from `Github Rest API`
+ */
+export interface GithubRepo {
 	name: string;
-	repository: string;
 	description: string;
-	npm: string;
+	homepage: string;
+	html_url: string;
+	language: string;
+	topics: string[];
+}
+
+/**
+ * Defines a Project data used to render the view.
+ */
+export interface Project extends Omit<GithubRepo, "html_url"> {
+	type: "app" | "package" | "website";
+	github: string;
+	packageName?: string;
 }
