0c7fdfada2d6ca131568d3ebbca14c0b44bccb44
Eric Rovell <ericrovell@gmail.com>
2021-11-07 14:49:25 +0300

    feat: update paths according to lang

--- src/core/paths.ts
+++ src/core/paths.ts
@@ -1,10 +1,12 @@
-import { base } from "$app/paths";
+import { derived } from "svelte/store";
+import { lang } from "./i18n";
 
-export const pathRoot = `${base}/`;
+export const root = derived(lang, $lang => `/${$lang}`);
 
-export const pathBlog = `${base}/blog`;
-export const pathGallery = `${base}/gallery`;
-export const pathProjects = `${base}/projects`;
-export const pathAbout = `${base}/about`;
+export const pathHome = derived(root, $root => `${$root}/home`);
+export const pathBlog = derived(root, $root => `${$root}/blog`);
+export const pathGallery = derived(root, $root => `${$root}/gallery`);
+export const pathProjects = derived(root, $root => `${$root}/projects`);
+export const pathAbout = derived(root, $root => `${$root}/about`);
 
-export const pathBlogpost = (slug: string) => `${base}/content/${slug}`;
+export const pathBlogpost = (slug: string) => `/content/${slug}`;
