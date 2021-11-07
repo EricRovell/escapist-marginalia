32fc3b8aa3d50ba7c5a282ba955be3ed9bbc2251
Eric Rovell <ericrovell@gmail.com>
2021-11-07 16:43:01 +0300

    feat(i18n): sync current lang pages

--- src/lib/components/LangSelect.svelte
+++ src/lib/components/LangSelect.svelte
@@ -1,16 +1,42 @@
 <script lang="ts">
+	import { page } from "$app/stores";
 	import { lang } from "@core/i18n";
 	import { Icon } from "@components";
 	import { iconLocale } from "@components/icons/default";
 
 	export let title: string | undefined = undefined;
-	// ^\/(ru|en)\/
+
+	/**
+	 * Improves UX by staying on the same page while changing the language.
+	 * The function cuts off /:lang parameter if present to construct from it
+	 * links to translated pages.
+	 * 
+	 * If the parameter is not present, redirect to the root.
+	 */
+	function stripLangParam(href: string): string {
+		const regex = /^\/(ru|en)\//;
+		return (regex.test(href) && href) 
+			? href.replace(regex, "")
+			: "home";
+	}
+
+	let href: string;
+	let path: string;
+
+	$: path = $page.path;
+	$: href = stripLangParam(path);
 </script>
 
 <div class="lang-select" {title}>
 	<Icon path={iconLocale} color="primary" />
-	<a href="/ru/home" class:active={$lang === "ru"}>RU</a>
-	<a href="/en/home" class:active={$lang === "en"}>EN</a>
+	{#each [ "en", "ru" ] as language}
+		<a
+			href={`/${language}/${href}`}
+			class:active={$lang === language}
+			>
+				{language}
+		</a>
+	{/each}
 </div>
 
 <style>
@@ -26,6 +52,10 @@
 		border: var(--border-1);
 		padding: 0.5em;
   }
+
+	.lang-select > a {
+		text-transform: uppercase;
+	}
 
 	.active {
 		color: var(--color-primary);
