b9e358622acf416051c9ef8ac42050d16a4b504b
Eric Rovell <ericrovell@gmail.com>
2021-11-24 02:47:02 +0300

    feat: using `ContentFilter` layout and page styles modules

--- src/routes/[lang]/blog/index.svelte
+++ src/routes/[lang]/blog/index.svelte
@@ -21,7 +21,9 @@
 </script>
 
 <script lang="ts">
+	import styles from "@styles/pages/blog.module.css";
 	import { Card } from "@components";
+	import { ContentFilter } from "@lib/layout";
 	import { pathBlogpost } from "@core/paths";
 	import { _, lang } from "@core/i18n";
 	import { groupBy } from "@lib/util";
@@ -29,49 +31,29 @@
 
 	export let posts: Blogpost[] = [];
 
+	let contentLanguage: string[] = [ $lang ];
 	let filteredPosts: Blogpost[];
 	let groupedPosts: Array<[ string, Blogpost[]]>;
 
-	$: filteredPosts = posts.filter(post => post.lang === $lang);
+	$: filteredPosts = posts.filter(post => contentLanguage.includes(post.lang));
 	$: groupedPosts = Object.entries(groupBy(filteredPosts, post => post.series));
 </script>
 
-<main>
-	{#each groupedPosts as [ series, posts ]}
-		<section>
-			<h2>
-				{$_(`categories.${series}`)}
-			</h2>
-			<div>
-				{#each posts as { title, slug, date }}
-					<Card
-						{title}
-						href={pathBlogpost(slug)}
-						{date}
-					/>
-				{/each}
-			</div>
-		</section>
-	{/each}
-</main>
-
-<style>
-	main {
-		display: flex;
-		flex-direction: column;
-		gap: var(--space-l);
-		width: 100%;
-	}
-
-	h2 {
-		font-size: var(--font-size-6);
-		margin-bottom: var(--space-m);
-		text-transform: capitalize;
-	}
-
-	section > div {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(325px, 1fr));
-		gap: var(--space-m);
-	}
-</style>
+<div class={styles.layout}>
+	<ContentFilter bind:contentLanguage>
+		{#each groupedPosts as [ series, posts ]}
+			<section>
+				<h2>{$_(`categories.${series}`)}</h2>
+				<div class={styles.posts}>
+					{#each posts as { title, slug, date }}
+						<Card
+							{title}
+							href={pathBlogpost(slug)}
+							{date}
+						/>
+					{/each}
+				</div>
+			</section>
+		{/each}
+	</ContentFilter>
+</div>
--- src/routes/[lang]/home.svelte
+++ src/routes/[lang]/home.svelte
@@ -21,18 +21,21 @@
 </script>
 
 <script lang="ts">
+	import styles from "@styles/pages/home.module.css";
 	import { pathBlogpost } from "@paths";
 	import { Link, Timeline, Datetime } from "@components";
+	import { ContentFilter } from "@lib/layout";
 	import { lang, _ } from "@core/i18n";
 	import { groupBy } from "@lib/util";
 	import type { Blogpost } from "../../types";
 
 	export let posts: Blogpost[] = [];
 
+	let contentLanguage: string[] = [ $lang ];
 	let filteredPosts: Blogpost[];
 	let groupedByYear: Record<number, Blogpost[]>;
 
-	$: filteredPosts = posts.filter(post => post.lang === $lang);
+	$: filteredPosts = posts.filter(post => contentLanguage.includes(post.lang));
 	$: groupedByYear = groupBy(filteredPosts, post => new Date(post.date).getFullYear());
 </script>
 
@@ -42,38 +45,33 @@
 	Posts from API are sorted by date. Nevertheless, after gouping them by a year,
 	object keys are sorted ASC. Have to reverse the entries.
 -->
-<main>
-	<Timeline.Container>
-	{#each Object.entries(groupedByYear).reverse() as [ year, posts ]}
-		<Timeline.Section>
-			<svelte:fragment slot="header">
-				<Datetime date={year} options={{ year: "numeric" }} />
-			</svelte:fragment>
-			{#each posts as { title, slug, date } (slug)}
-				<Timeline.Item>
-					<article>
-						<footer>
-							<Datetime {date} locale={$lang} options={{ month: "long", day: "numeric" }} />, {$_("message.blogpost")}
-						</footer>
-						<Link	href={pathBlogpost(slug)}>
-							{title}
-						</Link>
-					</article>
-				</Timeline.Item>
+<div class={styles.layout}>
+	<ContentFilter bind:contentLanguage>
+		<Timeline.Container>
+			{#each Object.entries(groupedByYear).reverse() as [ year, posts ]}
+				<Timeline.Section>
+					<svelte:fragment slot="header">
+						<Datetime date={year} options={{ year: "numeric" }} />
+					</svelte:fragment>
+					{#each posts as { title, slug, date } (slug)}
+						<Timeline.Item>
+							<article>
+								<footer>
+									<Datetime {date} locale={$lang} options={{ month: "long", day: "numeric" }} />, {$_("message.blogpost")}
+								</footer>
+								<Link	href={pathBlogpost(slug)}>
+									{title}
+								</Link>
+							</article>
+						</Timeline.Item>
+					{/each}
+				</Timeline.Section>
 			{/each}
-		</Timeline.Section>
-	{/each}
-	</Timeline.Container>
-</main>
+		</Timeline.Container>
+	</ContentFilter>
+</div>
 
 <style>
-	main {
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		padding: 0 var(--space-m);
-	}
-
 	article {
 		display: flex;
 		flex-direction: column;
