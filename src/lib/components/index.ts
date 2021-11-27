cc8a176de6b48dcab366221c697b976b839801c0
Eric Rovell <ericrovell@gmail.com>
2021-11-28 01:56:07 +0300

    chore: add `Code` export

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -11,5 +11,6 @@ export { default as LangSelect } from "./LangSelect.svelte";
 export { default as Link } from "./Link.svelte";
 export { default as CheckboxGroup } from "./CheckboxGroup.svelte";
 export { default as TOC } from "./TOC.svelte";
+export { default as Code } from "./code-highlight/Code.svelte";
 
 export * as Timeline from "./timeline";
