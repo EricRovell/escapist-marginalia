15aa3398ae1e6ad1d4b407a3ef9cf63159036647
Eric Rovell <ericrovell@gmail.com>
2021-11-28 01:52:54 +0300

    chore: move `Code` component to `components` dir

---
+++ src/lib/components/code-highlight/Code.svelte
@@ -0,0 +1,15 @@
+<script>
+	import "./code-highlight-dark.css";
+</script>
+
+<!--
+	@component
+	
+	Custom `code` html element to use in blogposts.
+	Provides a syntax highlight.
+
+	! Highlight styles are global, should be careful.
+-->
+<code>
+	<slot />
+</code>
