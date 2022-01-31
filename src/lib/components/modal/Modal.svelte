294ef6271e5584439da31bae9d04fe00a68be9fe
Eric Rovell <ericrovell@gmail.com>
2022-01-31 13:25:01 +0300

    feat: "close" event, using `inset`, add aria role

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { createEventDispatcher } from "svelte";
 	import { fade } from "svelte/transition";
 	import { portal } from "@lib/actions";
 	import Icon from "../icons/Icon.svelte";
@@ -6,22 +7,29 @@
 	import { preventPageScroll } from "@lib/util";
 	import styles from "./modal.module.css";
 
+	const dispatch = createEventDispatcher();
+
 	export let open = false;
 
-	const closeModal = () => open = false;
-	const closeModalEscape = (event: KeyboardEvent) => (event.key === "Escape") && closeModal();
+	const closeModal = () => {
+		open = false;
+		dispatch("close");
+	};
+
+	const handleKeydown = (event: KeyboardEvent) => (event.key === "Escape") && closeModal();
 	
 	$: preventPageScroll(open);
 </script>
 
 <svelte:window
-	on:keydown={closeModalEscape}
+	on:keydown={handleKeydown}
 />
 
 {#if open}
-	<div class={styles.modal} in:fade out:fade use:portal hidden>
+	<div class={styles.modal} in:fade out:fade use:portal hidden role="dialog" aria-modal="true">
 		<div class={styles.backdrop} on:click={closeModal} />
-		<button shape="square" on:click={closeModal}>
+		<!-- svelte-ignore a11y-autofocus -->
+		<button shape="square" on:click={closeModal} autofocus>
 			<Icon path={iconCancel} size="1.25rem" />
 		</button>
 		<div class={styles.contents}>
