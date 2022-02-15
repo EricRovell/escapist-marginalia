f6e668510fd7fe89b546afe868b22ccc1c457ac5
Eric Rovell <ericrovell@gmail.com>
2022-02-15 15:33:29 +0300

    refactor: move redirect into `Load`

--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { browser } from "$app/env";
-	import { goto } from "$app/navigation";
-	import { lang } from "@core/i18n";
-
-	if (browser) {
-		void goto(`${$lang}/home`, { replaceState: true });
-	}
-</script>
