458361b2f2c4bb8380eb1635d5496a0a7d8495d7
Eric Rovell <ericrovell@gmail.com>
2022-03-21 11:42:34 +0300

    refactor: update gallery route, use modal as query route

--- src/routes/[locale]/gallery/index.svelte
+++ src/routes/[locale]/gallery/index.svelte
@@ -1,17 +1,58 @@
-<script lang="ts">
-	import { PageMeta, Gallery } from "@components";
+<script context="module" lang="ts">
+	import type { Load } from "@sveltejs/kit";
 
-	import items from "@core/gallery.json";
+	export const load: Load = async ({ fetch }) => {
+		const res = await fetch("/api/photos.json");
+		const photos: GalleryItem[] = await res.json();
+
+		if (res.ok) {
+			return {
+				props: {
+					photos
+				}
+			};
+		}
+
+		return {
+			status: res.status,
+			error: new Error("Could not load URL")
+		};
+	};
+</script>
+
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { PageMeta, Gallery, Image, Modal } from "@components";
+	import { fetchJSON } from "@lib/util";
+	import type { GalleryItem } from "@components";
+
+	export let photos: GalleryItem[] = [];
+
+	$: id = $page.url.searchParams.get("id");
+
+	const getModalImage = async (id: string) => (
+		await fetchJSON<GalleryItem>(`/api/photos/${id}.json`)
+	);
 </script>
 
 <PageMeta route="gallery" />
 
 <main>
 	<Gallery
-		{items}
+		items={photos}
 		getSrc={({ path, format }) => `${path}.${format}`}
 	/>
 </main>
+
+<Modal open={Boolean(id)} on:close={() => window.history.go(-1)}>
+	{#await getModalImage(id) then photo}
+		<Image
+			className={"modal-image"}
+			src={`${photo.path}.${photo.format}`}
+			{...photo}
+		/>
+	{/await}
+</Modal>
 
 <style>
 	main {
