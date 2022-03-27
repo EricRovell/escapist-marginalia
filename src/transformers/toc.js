6cc60af480d545efc7c8e134a450a2e8d69f3eeb
Eric Rovell <ericrovell@gmail.com>
2022-03-27 14:31:48 +0300

    refactor: move md plugins into dir

---
+++ src/transformers/toc.js
@@ -0,0 +1,30 @@
+/**
+* Custom remark plugin for injecting information about `headings` into frontmatter.
+*/
+export const transformerTOC = () => {
+	let visit;
+	let tree_to_string;
+	return async function transformer(tree, vFile) {
+		if (!visit) {
+			tree_to_string = (await import("mdast-util-to-string")).toString;
+			visit = (await import("unist-util-visit")).visit;
+		}
+
+		vFile.data.toc = [];
+		
+		visit(tree, "heading", (node) => {
+			const title = tree_to_string(node);
+			vFile.data.toc.push({
+				level: node.depth,
+				title,
+				id: title.toLowerCase().replace(/\s/g, "-")
+			});
+		});
+
+		if (!vFile.data.fm) {
+			vFile.data.fm = {};
+		}
+
+		vFile.data.fm.toc = vFile.data.toc;
+	};
+};
