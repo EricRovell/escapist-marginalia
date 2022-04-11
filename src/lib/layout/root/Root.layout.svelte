a01545e2ea7f5fb257ede7a85b4f3db26c1ff0bf
Eric Rovell <ericrovell@gmail.com>
2022-04-11 12:54:58 +0300

    refactor: root layout structure

---
+++ src/lib/layout/root/Root.layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Masthead, Footer } from "@components";
+
+  import "@styles/utility.css";
+  import "@styles/tokens.css";
+  import "@styles/normalize.css";
+  import "@styles/theme-dark.css";
+  import "@styles/theme-light.css";
+  import "@styles/typography.css";
+
+  import styles from "./root.module.css";
+</script>
+
+<div class={styles.app}>
+  <Masthead />
+	<div class={styles.content}>
+		<slot />
+	</div>
+	<Footer />
+</div>
