2a997a92628c2901c2e5f5c50d6966d4bd770bc6
Eric Rovell <ericrovell@gmail.com>
2022-04-11 12:56:34 +0300

    refactor: mastehead component dir structure

---
+++ src/lib/components/masthead/Masthead.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { pathHome, pathBlog, pathGallery, pathProjects, pathAbout } from "@paths";
+	import { t } from "@core/i18n";
+
+	import * as Navigation from "../menu";
+	import Icon from "../icons/Icon.svelte";
+	import { ThemeSelect } from "../theme-select";
+	import LangSelect from "../LangSelect.svelte";
+	import Link from "../Link.svelte";
+	import { logo } from "@components/icons/logo";
+
+	import styles from "./masthead.module.css";
+</script>
+
+<header class={styles.masthead}>
+	<section label="logo">
+		<Link href={$pathHome}>
+			<Icon path={logo} size="25px" />
+		</Link>
+	</section>
+	<section label="navigation">
+		<Navigation.Container>
+			<Navigation.Item href={$pathHome} pattern={/\/home/}>
+				{$t("sections.home")}
+			</Navigation.Item>
+			<Navigation.Item href={$pathBlog} pattern={/\/blog|\/content/}>
+				{$t("sections.blog")}
+			</Navigation.Item>
+			<Navigation.Item href={$pathGallery} pattern={/\/gallery/}>
+				{$t("sections.gallery")}
+			</Navigation.Item>
+			<Navigation.Item href={$pathProjects}>
+				{$t("sections.projects")}
+			</Navigation.Item>
+			<Navigation.Item href={$pathAbout} disabled>
+				{$t("sections.about")}
+			</Navigation.Item>
+		</Navigation.Container>
+	</section>
+	<section label="controls">
+		<LangSelect />  
+		<ThemeSelect />
+	</section>
+</header>
