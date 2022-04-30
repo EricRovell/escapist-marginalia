d7077c0e400e9b34cda0c4954535c06ad775014d
Eric Rovell <ericrovell@gmail.com>
2022-04-30 23:35:40 +0300

    refactor: use shadow endpoint

--- src/routes/[locale]/home.svelte
+++ src/routes/[locale]/home.svelte
@@ -1,37 +1,5 @@
 <script context="module" lang="ts">
-	import type { Load } from "@sveltejs/kit";
 	import type { Blogpost, Project, GalleryItem } from "../../types";
-
-	export const load: Load = async ({ fetch }) => {
-
-		const requests = [
-			fetch("/api/posts"),
-			fetch("/api/photos"),
-			fetch("/api/projects")
-		];
-
-		const responses = await Promise.all(requests);
-		const [ posts, photos, projects ] = await Promise.all(responses.map(r => r.json()));
-
-		if (responses.every(r => r.ok)) {
-			return {
-				props: {
-					posts: posts.slice(0, 5),
-					photos: photos.slice(0, 4),
-					projects: projects.slice(0, 4)
-				}
-			};
-		}
-
-		for (let response of responses) {
-			if (!response.ok) {
-				return {
-					status: response.status,
-					error: new Error("Could not load URL")
-				};
-			}
-		}
-	};
 </script>
 
 <script lang="ts">
@@ -41,7 +9,7 @@
 	import { t } from "@core/i18n";
 	import styles from "./.home.module.css";
 
-	export let posts: Blogpost[] = [];
+	export let blogposts: Blogpost[] = [];
 	export let photos: GalleryItem[] = [];
 	export let projects: Project[] = [];
 </script>
@@ -67,7 +35,7 @@
 					</Link>
 				</header>
 				<ul class="grid-flexible" style="--item-size: 250px">
-					{#each posts as { created, description, keywords, title, slug }}
+					{#each blogposts as { created, description, keywords, title, slug }}
 						<li>
 							<CardArticle
 								{created}
