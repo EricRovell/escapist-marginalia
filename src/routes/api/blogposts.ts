4f85fbd3879d4feca6f060d3d82789d3c15fb390
Eric Rovell <ericrovell@gmail.com>
2022-04-30 23:34:43 +0300

    refactor: update posts api, rename to blogposts

---
+++ src/routes/api/blogposts.ts
@@ -0,0 +1,18 @@
+import { dev } from "$app/env";
+import { getBlogposts } from "@data/posts";
+import type { Blogpost } from "@types";
+
+export async function get({ url }) {
+	const published = !dev
+		? { published: true }
+		: {};
+
+	const data: Blogpost[] = await getBlogposts({
+		lang: url.searchParams.get("lang"),
+		...published
+	});
+
+	return {
+		body: data
+	};
+}
