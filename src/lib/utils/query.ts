1a874d3871f893f1f5e2676c0495b78d8cfa19f3
Eric Rovell <ericrovell@gmail.com>
2022-05-12 01:25:15 +0300

    feat: improve find utility

--- src/lib/utils/query.ts
+++ src/lib/utils/query.ts
@@ -7,34 +7,48 @@ type Matcher<T> = (value: T) => boolean;
  * - validator: validates a query option value;
  * - matcher: a curried function to match an item.
  */
-export interface QueryOption<T, Item> {
+export interface QueryItem<T, Item> {
 	value?: T;
 	validator: (value: T) => boolean;
 	matcher: (value: T) => Matcher<Item>;
 }
 
+export interface Query<T> {
+	[key: string]: QueryItem<unknown, T>;
+}
+
 export interface QueryOptions<T> {
-	[key: string]: QueryOption<unknown, T>;
+	limit?: number;
+	sort?: (a: T, b: T) => 1 | -1;
 }
 
 /**
  * Filters an array of items depending on a query.
  */
-export function find<T>(query: T[], options: QueryOptions<T>) {
+export function find<T>(items: T[], query: Query<T>, { limit, sort }: QueryOptions<T> = {}) {
+	let result: T[] = [];
 	const matchers: Matcher<T>[] = [];
 
-	for (const key of Object.keys(options)) {
-		const { value, validator, matcher } = options[key];
+	for (const key of Object.keys(query)) {
+		const { value, validator, matcher } = query[key];
 		if (value && validator(value)) {
 			matchers.push(matcher(value));
 		}
 	}
 
-	return query.filter(item => {
+	result = items.filter(item => {
 		for (const matcher of matchers) {
 			if (!matcher(item)) return false;
 		}
 
 		return true;
 	});
+
+	if (sort) {
+		result.sort((a, b) => sort(a, b));
+	}
+
+	return limit
+		? result.slice(0, limit)
+		: result;
 }
