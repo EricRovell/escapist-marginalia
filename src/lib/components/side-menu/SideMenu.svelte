73319a7a206494798f1e9dd74791f4e64b973db4
Eric Rovell <ericrovell@gmail.com>
2022-05-18 01:15:58 +0300

    feat: SideMenu component

---
+++ src/lib/components/side-menu/SideMenu.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { afterNavigate } from "$app/navigation";
+	import SideMenuBody from "./SideMenuBody.svelte";
+	import SideMenuSwitch from "./SideMenuSwitch.svelte";
+	import styles from "./side-menu.module.css";
+
+	export let size = "2rem";
+	let show = false;
+
+	const switchMenu = () => show = !show;
+
+	afterNavigate(() => show = false);
+</script>
+
+<aside class={styles.wrapper} class:show style:--switch-size="{size}">
+	<div class={styles.backdrop} class:show on:click={switchMenu} />
+	<SideMenuBody bind:show>
+		<slot />
+	</SideMenuBody>
+	<SideMenuSwitch
+		bind:show
+		on:click={switchMenu}
+	>
+		<slot name="title" />
+	</SideMenuSwitch>
+</aside>
