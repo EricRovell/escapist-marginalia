66c85ed29347305a7863baf6a6dd3dbb982df00d
Eric Rovell <ericrovell@gmail.com>
2022-08-19 13:22:14 +0300

    fix: remove url search params as static build crashes

--- src/routes/[locale]/gallery/+page.ts
+++ src/routes/[locale]/gallery/+page.ts
@@ -1,9 +1,9 @@
 import { getGalleryItems } from "@data/photos";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = ({ url }) => {
-	let limit = Number(url.searchParams.get("limit"));
-	limit = (!Number.isNaN(limit) && limit > 0) ? limit : 50;
-	const data = getGalleryItems(limit);
+export const load: PageLoad = () => {
+	//let limit = Number(url.searchParams.get("limit"));
+	//limit = (!Number.isNaN(limit) && limit > 0) ? limit : 50;
+	const data = getGalleryItems();
 	return data;
 };
