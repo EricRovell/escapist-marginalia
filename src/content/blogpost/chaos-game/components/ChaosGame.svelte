cffbee1d54abac5083aa51e852895bf0ab20747f
Eric Rovell <ericrovell@gmail.com>
2022-09-05 15:12:04 +0300

    feat(blogpost): Chaos component

---
+++ src/content/blogpost/chaos-game/components/ChaosGame.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Canvas, CanvasLayer } from "@components";
+	import { Chaos, Point, Polygon } from "../chaos";
+	import type { PolygonOrigin, ChaosOptions } from "../chaos/chaos.types";
+
+	export let distances = [];
+	export let height = 350;
+	export let origin: PolygonOrigin = undefined;
+	export let palette: ChaosOptions["palette"] | undefined = undefined;
+	export let points = 2500;
+	export let pointSize = 1;
+	export let polygon = 3;
+	export let scale = 250;
+	export let step = { value: 0.5, factor: true };
+	export let width = 350;
+</script>
+
+<figure>
+	<Canvas {width} {height}>
+		<CanvasLayer
+			id="chaos-game"
+			setup="{({ context, height, width }) => {
+				context.translate(width / 2, height / 2);
+			}}"
+			draw="{({ context }) => {
+				const point = new Point({ x: 0, y: 0 }, {
+					size: pointSize
+				});
+				const poly = new Polygon(polygon, scale, origin);
+				const chaos = new Chaos(poly, { palette, step, distances });
+				
+				for (let i = 0; i <= points; i++) {
+					const { position, color } = chaos.move();
+					context.fillStyle = color;
+					point.coords = position;
+					point.render(context);
+				}
+			}}"
+		/>
+	</Canvas>
+	{#if $$slots.default}
+		<figcaption>
+			<slot />
+		</figcaption>
+	{/if}
+</figure>
+
+<style>
+	figure {
+		--canvas-bg: var(--surface-2);
+
+		display: grid;
+		gap: var(--space-s);
+		justify-items: center;
+		/* width: min-content; */
+	}
+
+	figure :global(canvas) {
+		/* width: 350px; */
+		justify-self: center;
+		border: 2px solid var(--color-border);
+		border-radius: var(--radius-l);
+	}
+
+	figure figcaption {
+		text-align: center;
+	}
+</style>
