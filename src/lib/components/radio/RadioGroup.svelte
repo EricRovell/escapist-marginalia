e1619f949d8d1e80d5392a458bc0918781232420
Eric Rovell <ericrovell@gmail.com>
2022-09-06 13:14:13 +0300

    feat(component): update Radio component

--- src/lib/components/radio/RadioGroup.svelte
+++ src/lib/components/radio/RadioGroup.svelte
@@ -4,12 +4,16 @@
 
 	export let className = "";
 	export let name: string;
-	export let legend: string;
+	export let legend: string | undefined = undefined;
 
-	setContext("name", {	name });
+	setContext("name", { name });
 </script>
 
 <fieldset class="{styles.fieldset} {className}" on:change>
-	<legend>{legend}</legend>
+	<legend>
+		<slot name="title">
+			{legend}
+		</slot>
+	</legend>
 	<slot />
 </fieldset>
