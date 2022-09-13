9b51e94709053daf2228fcef858b2a11a8d7cc93
Eric Rovell <ericrovell@gmail.com>
2022-09-13 20:35:14 +0300

    feat(blogpost): update aperture blogpost & refactor code structure

---
+++ src/content/blogpost/where-do-aperture-numbers-come-from/interactive/Diaphragm.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Range } from "@components";
+	import { Diaphragm } from "../components";
+	import styles from "./diaphragm.module.css";
+
+	export let t: Record<string, string>;
+
+	let angle = 45;
+</script>
+
+<!--
+	Iris Diaphragm Mechanism Demonstration.
+-->
+<section class="wide">
+	<h3>{t["title"]}</h3>
+	<Diaphragm bind:angle />
+	<form class="{styles.controls}">
+		<Range bind:value={angle} min={45} max={85}>
+			{t["angle"]}
+		</Range>
+	</form>
+</section>
---
+++ src/content/blogpost/where-do-aperture-numbers-come-from/interactive/FNumbersScale.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" context="module">
+	import { Range } from "@components";
+	import FNumbers from "../components/FNumbers.svelte";
+</script>
+
+<script lang="ts">
+	import styles from "./f-numbers-scale.module.css";
+
+	export let t: Record<string, string>;
+
+	/**
+	 * range of fstops to display
+	 */
+	export let fstopFrom = 0;
+	export let fstopTo = 7;
+
+	const fstopMin = 0;
+	const fstopMax = 12;
+</script>
+
+<!--
+	Demonstrates the f-numbers scale.
+-->
+<section class="wide {styles.wrapper}">
+	<h3>{t["title"]}</h3>
+	<FNumbers
+		{fstopFrom}
+		{fstopTo}
+		{t}
+	/>
+	<form on:submit|preventDefault>
+		<Range bind:value={fstopTo} min={fstopMin + 1} max={fstopMax}>
+			{t["f-numbers"]}
+		</Range>
+	</form>
+</section>
