f598bb93a79c067ab4704d66981310f9968e4206
Eric Rovell <ericrovell@gmail.com>
2022-09-19 01:19:12 +0300

    feat(component): implement Drawer

---
+++ src/lib/components/drawer/Drawer.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { afterNavigate } from "$app/navigation";
+	import { preventPageScroll } from "$lib/utils/helpers";
+	import { swipable, type SwipeEvent } from "$lib/actions";
+	import styles from "./drawer.module.css";
+
+	export let className = "";
+	/**
+	 * Should content be hidden when threshold is not met.
+	 */
+	export let hidden = false;
+	export let show = false;
+
+	export const toggle = () => {
+		show = !show;
+	};
+
+	const handleSwipe = (event: CustomEvent<SwipeEvent>) => {
+		if (event.detail.direction === "down") {
+			show = false;
+		}
+	};
+	
+	afterNavigate(() => {
+		show = false;
+	});
+
+	$: preventPageScroll(show);
+</script>
+
+<div
+	class="{styles.wrapper} {className}"
+	class:show
+	class:hidden
+	use:swipable={{ threshold: 50, timeout: 1000 }}
+	on:swipe-down={handleSwipe}
+>
+	<div
+		class={styles.backdrop}
+		class:show
+		on:click={toggle}
+	/>
+	<article
+		class={styles["menu-body"]}
+		class:show
+		label="drawer"
+	>
+		<slot />
+	</article>
+	<slot
+		name="toggle"
+		{toggle}
+		{show}
+	/>
+</div>
