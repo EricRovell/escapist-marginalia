38f5779bbe1294395463387b2e9f2af7d6d6cf8f
Eric Rovell <ericrovell@gmail.com>
2022-09-19 01:11:43 +0300

    feat(component): inplement menu indicator

---
+++ src/lib/components/menu-indicator/MenuIndicator.svelte
@@ -0,0 +1,11 @@
+<script>
+	import styles from "./menu-indicator.module.css";
+
+	export let active = false;
+</script>
+
+<div class={styles["indicator"]} class:active>
+	<span />
+	<span />
+	<span />
+</div>
