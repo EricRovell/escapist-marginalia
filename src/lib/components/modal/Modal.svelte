a070cca9a002c4b17fd6743f9ae74f527e31ad4d
Eric Rovell <ericrovell@gmail.com>
2022-09-20 00:58:03 +0300

    refactor(component): use shortcut action

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -1,36 +1,43 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import { fade } from "svelte/transition";
-	import { portal } from "@lib/actions";
+	import { portal, shortcut } from "@lib/actions";
 	import Icon from "../icons/Icon.svelte";
 	import { iconCancel } from "../icons/default";
 	import { preventPageScroll } from "@utils/helpers";
 	import styles from "./modal.module.css";
 
-	const dispatch = createEventDispatcher();
-
 	export let open = false;
+
+	const dispatch = createEventDispatcher();
 
 	const closeModal = () => {
 		open = false;
 		dispatch("close");
 	};
-
-	const handleKeydown = (event: KeyboardEvent) => (event.key === "Escape") && closeModal();
 	
 	$: preventPageScroll(open);
 </script>
 
-<svelte:window
-	on:keydown={handleKeydown}
-/>
-
 {#if open}
-	<div class={styles.modal} in:fade out:fade use:portal hidden role="dialog" aria-modal="true">
+	<div
+		aria-modal="true"
+		class={styles.modal}
+		hidden
+		in:fade
+		out:fade
+		role="dialog"
+		use:portal
+		use:shortcut={{
+			active: open,
+			code: "Escape",
+			callback: closeModal
+		}}
+	>
 		<div class={styles.backdrop} on:click={closeModal} />
 		<!-- svelte-ignore a11y-autofocus -->
 		<button shape="square" on:click={closeModal} autofocus>
-			<Icon path={iconCancel} size="1.25rem" />
+			<Icon path={iconCancel} />
 		</button>
 		<div class={styles.contents}>
 			<slot />
