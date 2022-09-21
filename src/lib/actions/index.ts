c523e29aab8246b9a4ca05e40aabd4126c10c32f
Eric Rovell <ericrovell@gmail.com>
2022-09-21 03:05:53 +0300

    refactor(action): move out `current` action from component

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,5 +1,6 @@
-export { portal } from "./portal";
+export { current } from "./current";
 export { pannable } from "./pannable";
+export { portal } from "./portal";
 export { shortcut } from "./shortcut";
 export { swipable } from "./swipable";
 
