ed0ef09a4966910ffc03ee270ae8a5733a8da4c1
Eric Rovell <ericrovell@gmail.com>
2022-09-23 01:16:41 +0300

    refactor: update Link component file structure

--- src/lib/components/link/Link.svelte
+++ src/lib/components/link/Link.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import styles from "./link.module.css";
+
 	export let block = false;
-	export let className: string | undefined = undefined;
+	export let className = "";
 	export let download: undefined | string = undefined;
 	export let href: string;
+	export let label: string | undefined = undefined;
 	export let targetBlank = false;
 	export let nofollow = false;
 	export let disabled = false;
@@ -16,7 +19,7 @@
 </script>
 
 <a
-	class={className}
+	class="{styles.link} {className}"
 	class:block
 	{download}
 	{href}
@@ -25,46 +28,10 @@
 	{title}
 	{...$$restProps}
 	aria-disabled={disabled ? "true" : undefined}
+	aria-label="{label}"
 	class:disabled
 	tabIndex={disabled ? -1 : undefined}
-	>
+	{...$$restProps}
+>
 		<slot />
 </a>
-
-<style>
-	a {
-		align-items: baseline;
-		color: var(--color-link);
-		font-size: inherit;
-	}
-
-	a:not(.disabled):hover {
-		color: var(--color-link-hover, currentColor);
-		text-decoration: underline;
-	}
-
-	a:focus-visible {
-		outline-color: var(--color-link-hover, currentColor);
-	}
-
-	a:active {
-		color: var(--color-link-active);
-		background-color: var(--color-link-active-bg);
-	}
-
-	a[aria-disabled="true"] {
-		cursor: not-allowed;
-		opacity: 0.7;
-    text-decoration: line-through;
-	}
-
-	a[aria-disabled="true"]:active {
-		pointer-events: none;
-	}
-
-	a.block {
-		display: flex;
-		align-items: center;
-		gap: var(--space-1-rel);
-	}
-</style>
