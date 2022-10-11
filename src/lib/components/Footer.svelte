a348ed631583c8643fd7f2823dd34a7e86e1704c
Eric Rovell <ericrovell@gmail.com>
2022-10-11 22:35:11 +0300

    feat: set header/footer border

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -69,7 +69,7 @@
 <style>
 	footer {
 		grid-area: footer;
-		
+
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -86,6 +86,7 @@
 		height: 100%;
 		padding-top: var(--space-7);
 		padding-bottom: var(--space-7);
+		border-top: 2px solid var(--color-border);
 
 		justify-self: center;
 	}
