24d20d86eedb875f3150d29e2ce24b667fab1ea7
Eric Rovell <ericrovell@gmail.com>
2022-10-18 23:00:30 +0300

    feat(blogpost): egyptian numerals

---
+++ src/content/blogpost/radix/interactive/egyptian-numerals/EgyptianNumerals.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { numerals } from "./egyptian.constants";
+	import styles from "./egyptian-numerals.module.css";
+</script>
+
+<ol class="{styles.numerals}">
+	{#each numerals as numeral, i}
+		<li>
+			<span class="{styles.icon}">{@html numeral}</span>
+			<span class="{styles.value}">{10 ** i}</span>
+		</li>
+	{/each}
+</ol>
