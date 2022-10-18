55f53512432d7ad3b290a793fa64c38d67c0d06f
Eric Rovell <ericrovell@gmail.com>
2022-10-18 23:01:14 +0300

    feat(blogpost): number into decimal interactive

--- src/content/blogpost/radix/interactive/index.ts
+++ src/content/blogpost/radix/interactive/index.ts
@@ -1,3 +1,6 @@
 export { default as DecimalConversionStages } from "./decimal-conversion-stages/DecimalConversionStages.svelte";
 export { default as DisassemblingNumber } from "./disassembling-number/DisassemblingNumber.svelte";
+export { default as EgyptianNumerals } from "./egyptian-numerals/EgyptianNumerals.svelte";
+export { default as EgyptianNumber } from "./egyptian-numerals/EgyptianNumber.svelte";
+export { default as NumberIntoDecimal } from "./number-into-decimal/NumberIntoDecimal.svelte";
 export { default as RomanNumerals } from "./roman-numerals/RomanNumerals.svelte";
