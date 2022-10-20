8ec47c80077fd999427ea98d8818b8810108e776
Eric Rovell <ericrovell@gmail.com>
2022-10-20 14:39:34 +0300

    feat(blogpost): Numerals component

---
+++ src/content/blogpost/radix/interactive/numerals/Numerals.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import styles from "./numerals.module.css";
+
+	export let codes: string[] = [];
+	export let calcValue = (i: number) => 10 ** i;
+</script>
+
+<!--
+	Renders the Unicode HTML codes.
+-->
+<ol class="{styles.numerals}">
+	{#each codes as numeral, i}
+		<li>
+			<span class="{styles.icon}">{@html numeral}</span>
+			<span class="{styles.value}">
+				{calcValue(i)}
+			</span>
+		</li>
+	{/each}
+</ol>
