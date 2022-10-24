38dc917391f98493aab54e6d0cbcfe6e7b8f02a2
Eric Rovell <ericrovell@gmail.com>
2022-10-24 22:46:52 +0300

    feat(blogpost): romans interactive validation

--- src/content/blogpost/radix/interactive/roman-numerals/RomanNumerals.svelte
+++ src/content/blogpost/radix/interactive/roman-numerals/RomanNumerals.svelte
@@ -6,20 +6,33 @@
 	export let number = 0;
 	export let t: Record<string, string>;
 
+	let valid = true;
+
 	$: roman = arabic2romans(number);
 </script>
 
 <section class="wide interactive">
 	<h4>{t.title}</h4>
 	<output class="{styles.romans}">
-		{#if roman}
+		{#if roman && valid}
 			{roman}
 		{:else}
 			N
 		{/if}
 	</output>
 	<form class="{styles.form}" on:submit|preventDefault>
-		<InputNumber bind:value="{number}" min="{1}" max="{3999}">
+		<InputNumber
+			bind:value="{number}"
+			bind:valid
+			min="{1}"
+			max="{3999}"
+			validators="{[
+				(value) => ({
+					valid: value > 0 && value < 4000,
+					message: t["invalid-value-message"]
+				})
+			]}"
+		>
 			{t.arabic}
 		</InputNumber>
 	</form>
