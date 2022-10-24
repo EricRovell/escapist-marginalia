392af5401976c617735d249791a13bf3f7ee5a6c
Eric Rovell <ericrovell@gmail.com>
2022-10-24 22:35:26 +0300

    feat: input fields validation state

--- src/lib/components/input-text/InputText.svelte
+++ src/lib/components/input-text/InputText.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { validate } from "@utils/helpers";
+	import type { Validator } from "@types";
+	import styles from "./input-text.module.css";
+
 	export let className = "";
 	export let id: string | undefined = undefined;
 	export let list: string | undefined = undefined;
@@ -12,8 +16,26 @@
 	export let size: number | undefined = undefined;
 	export let spellcheck = false;
 	export let value = "";
+	export let validators: Validator<string>[] = [];
 
-	import styles from "./input-text.module.css";
+	let validity = {
+		dirty: false,
+		valid: true,
+		message: ""
+	};
+
+	// for parent use
+	export let valid = validity.valid;
+
+	const handleInput = (e: InputEvent) => {
+		if (!validity.dirty) {
+			validity.dirty = true;
+		}
+
+		const target = e.target as HTMLInputElement;
+		validity = validate(target.value, ...validators);
+		valid = validity.valid;
+	};
 </script>
 
 <label class="{styles.label} {className}">
@@ -21,11 +43,14 @@
 	<input
 		bind:value
 		class="{styles.input}"
+		class:valid="{validity.dirty && validity.valid}"
+		class:invalid="{validity.dirty && !validity.valid}"
 		{id}
 		{list}
 		{maxlength}
 		{minlength}
 		{name}
+		on:input="{handleInput}"
 		on:input
 		{pattern}
 		{placeholder}
@@ -35,5 +60,10 @@
 		{spellcheck}
 		type="text"
 	/>
+	{#if validity.dirty && !validity.valid}
+		<span class="{styles["error-message"]}">
+			{validity.message}
+		</span>
+	{/if}
 	<slot name="datalist" />
 </label>
