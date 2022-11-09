f49dedcf57df321ef89cfeb686a06be37c7c5d7c
Eric Rovell <ericrovell@gmail.com>
2022-11-09 13:39:10 +0300

    feat: define project page layout

---
+++ src/lib/layout/project/ProjectHeader.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Link, Icon } from "@components";
+	import { iconGithub, iconNpm, iconURL } from "$lib/components/icons/default";
+	import type { Project } from "@types";
+	import styles from "./project-header.module.css";
+
+	export let description: Project["description"];
+	export let name: Project["name"];
+	export let npm: Project["npm"];
+	export let repository: Project["repository"];
+	export let techstack: Project["techstack"] = [];
+	export let website: Project["website"];
+
+	const links = [
+		{ href: repository, title: "Github", icon: iconGithub },
+		{ href: npm, title: "NPM", icon: iconNpm },
+		{ href: website, title: "Website", icon: iconURL }
+	].filter(({ href }) => href);
+</script>
+
+<div class={styles.wrapper}>
+	<h1>
+		{name}
+	</h1>
+	<p>{description}</p>
+	<footer>
+		<ul class="{styles.keywords}">
+			{#each techstack as keyword}
+				<li class="surface-4">
+					{keyword}
+				</li>
+			{/each}
+		</ul>
+		<ul class="{styles.links}">
+			{#each links as { href, icon, title }}
+				<li>
+					<Link {href} {title}>
+						<Icon path="{icon}" />
+						{title}
+					</Link>
+				</li>
+			{/each}
+		</ul>
+	</footer>
+</div>
