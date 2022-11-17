bba07e988032c30710a5c3f44ab5db7eb8a87dbb
Eric Rovell <ericrovell@gmail.com>
2022-11-17 15:14:47 +0300

    feat: sketch layout

---
+++ src/lib/layout/sketch/Sketch.svelte
@@ -0,0 +1,56 @@
+<script context="module">
+	import { Link as a } from "@components";
+	export { a };
+</script>
+
+<script>
+	import { webpage } from "@core/paths";
+	import { Meta } from "@components";
+	import Header from "./SketchHeader.svelte";
+	import { LayoutPage } from "../page-header";
+	import styles from "./sketch.module.css";
+
+	export let description;
+	export let keywords = [];
+	export let lang;
+	export let title;
+	export let updated;
+</script>
+
+<Meta
+	title="{title}"
+	meta={{
+		description,
+		keywords,
+		language: lang
+	}}
+	openGraph={{
+		title,
+		description,
+		locale: lang,
+		type: "article",
+		tag: keywords,
+		section: "project",
+		site_name: "Eric Rovell",
+		author: "Eric Rovell",
+		url: $webpage,
+		"article:modified_time": updated
+	}}
+	twitter={{
+		card: "summary",
+		title,
+		description,
+		url: $webpage
+	}}
+/>
+
+<LayoutPage className="{styles.main}" wide>
+	<Header
+		{description}
+		{title}
+		slot="banner"
+	/>
+	<article class={styles.article}>
+		<slot />
+	</article>
+</LayoutPage>
---
+++ src/lib/layout/sketch/SketchHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import styles from "./sketch-header.module.css";
+
+	export let description: string;
+	export let title: string;
+</script>
+
+<div class={styles.wrapper}>
+	<h1>
+		{title}
+	</h1>
+	<p>{description}</p>
+	<slot />
+</div>
