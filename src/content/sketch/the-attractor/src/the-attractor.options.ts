1f27e2c956cff92520bdb8b66abd9110fb14d5ce
Eric Rovell <ericrovell@gmail.com>
2022-11-20 01:04:42 +0300

    feat: the attractor sketch

---
+++ src/content/sketch/the-attractor/src/the-attractor.options.ts
@@ -0,0 +1,11 @@
+export interface Options {
+	attractors: number;
+	particles: number;
+	lifetime: number;
+}
+
+export const options: Options = {
+	attractors: 3,
+	particles: 1200,
+	lifetime: 1200
+};
