1aa36ef69ad25e14da29163d3e0abf7a149977df
Eric Rovell <ericrovell@gmail.com>
2022-12-04 14:16:02 +0300

    chore: expose more canvas types

--- src/lib/components/canvas/index.ts
+++ src/lib/components/canvas/index.ts
@@ -1,3 +1,3 @@
 export { default as Canvas } from "./Canvas.svelte";
 export { default as CanvasLayer } from "./Layer.svelte";
-export type { Renderer } from "./canvas.types";
+export type { Renderer, RenderArguments } from "./canvas.types";
