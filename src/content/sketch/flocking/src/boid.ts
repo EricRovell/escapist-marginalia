163b19bf83ae9573a0d784e65949ea63bc4259aa
Eric Rovell <ericrovell@gmail.com>
2022-12-19 03:04:20 +0300

    feat(sketch): using qtree & more options

--- src/content/sketch/flocking/src/boid.ts
+++ src/content/sketch/flocking/src/boid.ts
@@ -2,11 +2,11 @@ import type { RenderArguments } from "@components/canvas";
 import { vector, type Vector } from "@ericrovell/vector";
 
 interface BoidOptions {
-	forceLimit: number;
-	mass: number;
-	scale: number;
+	forceLimit?: number;
+	mass?: number;
+	scale?: number;
 	separation: number;
-	speedLimit: number;
+	speedLimit?: number;
 	x: number;
 	y: number;
 }
@@ -14,6 +14,7 @@ interface BoidOptions {
 interface BehaviourCoef {
 	align: number;
 	cohesion: number;
+	perception: number;
 	separate: number;
 }
 
@@ -29,164 +30,111 @@ export class Boid {
 	x: BoidOptions["x"];
 	y: BoidOptions["y"];
 
-	constructor({ forceLimit, mass, scale, separation, speedLimit, x, y }: BoidOptions) {
+	constructor({ forceLimit = 0.1, mass = 1, scale = 5, separation, speedLimit = 3.5, x, y }: BoidOptions) {
 		this.acceleration = vector(0, 0);
 		this.forceLimit = forceLimit;
 		this.mass = mass;
 		this.position = vector(x, y);
 		this.scale = scale;
 		this.separation = separation;
-		this.speed = vector(0, 0);
+		this.speed = vector(0, 0).random().setMagnitude(Math.random() * speedLimit);
 		this.speedLimit = speedLimit;
 		this.x = x;
 		this.y = y;
 	}
 
 	applyBehaviours(boids: Boid[], coef: BehaviourCoef): void {
-		const separateForce = this.separate(boids);
-		const alignForce = this.align(boids);
-		const cohesionForce = this.cohesion(boids);
+		let count = 0;
 
-		// making forces stronger via sliders
-		alignForce.scaleSelf(coef.align);
-		cohesionForce.scaleSelf(coef.cohesion);
-		separateForce.scaleSelf(coef.separate);
+		const alignForce = vector(0, 0);
+		const cohesionForce = vector(0, 0);
+		const separationForce = vector(0, 0);
 
-		this.applyForce(separateForce);
-		this.applyForce(alignForce);
-		this.applyForce(cohesionForce);
+		for (const boid of boids) {
+			const dist = this.position.distance(boid.position);
+
+			if (boid !== this && dist < coef.perception) {
+				alignForce.addSelf(boid.speed);
+				cohesionForce.addSelf(boid.position);
+				separationForce.addSelf(
+					// steering
+					this.position.sub(boid.position).scaleSelf(dist ** 2, true)
+				);
+
+				count++;
+			}
+		}
+
+		if (count) {
+			alignForce
+				.scaleSelf(count, true)
+				.setMagnitudeSelf(this.speedLimit)
+				.subSelf(this.speed)
+				.limitSelf(this.forceLimit)
+				.scaleSelf(coef.align);
+
+			cohesionForce
+				.scaleSelf(count, true)
+				.subSelf(this.position)
+				.setMagnitudeSelf(this.speedLimit * 0.75)
+				.subSelf(this.speed)
+				.limitSelf(this.forceLimit)
+				.scaleSelf(coef.cohesion);
+
+			separationForce
+				.scaleSelf(count, true)
+				.setMagnitudeSelf(this.speedLimit)
+				.subSelf(this.speed)
+				.limitSelf(this.forceLimit)
+				.scaleSelf(coef.separate);
+		}
+
+		this.applyForces(alignForce, cohesionForce, separationForce);
 	}
 
-	applyForce(force: Vector): void {
-		this.acceleration.addSelf(force.scale(1 / this.mass));
+	applyForces(...forces: Vector[]): void {
+		for (const force of forces) {
+			this.acceleration.addSelf(
+				force.scale(this.mass, true)
+			);
+		}
 	}
 
 	move(): void {
+		this.position.addSelf(this.speed);
 		this.speed.addSelf(this.acceleration);
 		this.speed = this.speed.limit(this.speedLimit);
-		this.position.addSelf(this.speed);
 		// check for the case if the force has stopped
 		this.acceleration.set(0, 0);
 	}
 
-	// Method checks for nearby boids and steers away
-	separate(boids: Boid[]): Vector {
-		let sum = vector(0, 0);
-		let count = 0;
-
-		// For every boid in the system, check if it is too close
-		for (let i = 0; i < boids.length; i++) {
-			const distance = this.position.distance(boids[i].position);
-			if (distance > 0 && distance < this.separation) {
-				// Calculate vector pointing away from neighbour
-				const steerAway = this.position.sub(boids[i].position);
-				steerAway.normalizeSelf();
-
-				// Weight by distance
-				if (distance) {
-					steerAway.scaleSelf(1 / distance);
-				}
-
-				sum.addSelf(steerAway);
-				count++;
+	handleBorders(width: number, height: number, bound = false) {
+		if (bound) {
+			if (this.position.x > width || this.position.x < 0) {
+				this.speed.x *= -1;
 			}
-		}
 
-		// Average from all close neighbours
-		if (count > 0) {
-			sum.scaleSelf(1 / count);
-			// Our desired vector is the average scaled to maxSpeed
-			sum.normalizeSelf();
-			sum.scaleSelf(this.speedLimit);
-			// Steering = Desired - Velocity
-			sum.subSelf(this.speed);
-			sum = sum.limit(this.forceLimit);
-		}
-
-		return sum;
-	}
-
-	align(boids: Boid[]): Vector {
-		const neighbourDist = 50;
-		const sum = vector(0, 0);
-		let count = 0;
-
-		for (let i = 0; i < boids.length; i++) {
-			const distance = this.position.distance(boids[i].position);
-			if (distance > 0 && distance < neighbourDist) {
-				// velocity vector sum of all close neighbours makes it align
-				sum.addSelf(boids[i].speed);
-				count++;
+			if (this.position.y > height || this.position.y < 0) {
+				this.speed.y *= -1;
 			}
-		}
-
-		if (count > 0) {
-			sum.scaleSelf(1 / count);
-			sum.normalizeSelf();
-			sum.scaleSelf(this.speedLimit);
-
-			const steer = sum
-				.sub(this.speed)
-				.limit(this.forceLimit);
-
-			return steer;
 		} else {
-			// no one close around => zero force
-			return vector(0, 0);
-		}
-	}
-
-	cohesion(boids: Boid[]): Vector {
-		const neighborDist = 50;
-		const sum = vector(0, 0);
-		let count = 0;
-
-		for (let i = 0; i < boids.length; i++) {
-			const distance = this.position.distance(boids[i].position);
-			if ((distance > 0) && (distance < neighborDist)) {
-				sum.addSelf(boids[i].position);
-				count++;
+			// makes the canvas like infinite world alike round earth
+			if (this.position.x < -this.scale) {
+				this.position.x = width + this.scale;
+			}
+			if (this.position.y < -this.scale) {
+				this.position.y = height + this.scale;
+			}
+			if (this.position.x > width + this.scale) {
+				this.position.x = -this.scale;
+			}
+			if (this.position.y > height + this.scale) {
+				this.position.y = -this.scale;
 			}
 		}
-
-		if (count > 0) {
-			// no normalizing => makes force stroger => makes gap closer
-			sum.scaleSelf(1 / count);
-			return this.seek(sum);
-		} else {
-			return vector(0, 0);
-		}
 	}
 
-	// makes seek the desired target
-	seek(target: Vector): Vector {
-		// Steering = Desired - Velocity
-		let desired = target.subSelf(this.position);
-		desired = desired.setMagnitude(this.speedLimit);
-
-		let steering = desired.subSelf(this.speed);
-		steering = steering.limit(this.forceLimit);
-
-		return steering;
-	}
-
-	handleBorders(width: number, height: number) {
-		// makes the canvas like infinite world alike round earth
-		if (this.position.x < -this.scale) {
-			this.position.x = width + this.scale;
-		}
-		if (this.position.y < -this.scale) {
-			this.position.y = height + this.scale;
-		}
-		if (this.position.x > width + this.scale) {
-			this.position.x = -this.scale;
-		}
-		if (this.position.y > height + this.scale) {
-			this.position.y = -this.scale;
-		}
-	}
-
-	render({ context, width, height }: RenderArguments) {
+	render({ context }: RenderArguments) {
 		const phi = this.speed.getPhi() + Math.PI / 2;
 
 		context.fillStyle = "rgb(127 127 127)";
@@ -204,7 +152,5 @@ export class Boid {
 		context.restore();
 		context.fill();
 		context.stroke();
-
-		this.handleBorders(width, height);
 	}
 }
--- src/content/sketch/flocking/src/flocking.options.ts
+++ src/content/sketch/flocking/src/flocking.options.ts
@@ -1,9 +1,27 @@
 export interface Options {
+	align: number;
+	bound: boolean;
+	capacity: number;
+	cohesion: number;
 	color: string;
 	boids: number;
+	perception: number;
+	scale: number;
+	separate: number;
+	showQTree: boolean;
+	qtree: boolean;
 }
 
 export const options: Options = {
+	align: 1,
+	bound: true,
+	cohesion: 1,
+	capacity: 4,
 	color: "rgb(255 255 255 / 0.05)",
-	boids: 75
+	boids: 125,
+	perception: 50,
+	scale: 3.5,
+	separate: 1,
+	showQTree: false,
+	qtree: true
 };
