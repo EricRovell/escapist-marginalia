38aec13c742d38e29f5d6623669041afb063d3b8
Eric Rovell <ericrovell@gmail.com>
2023-01-08 00:16:49 +0300

    feat(component): implement Select

---
+++ src/lib/components/input-select/InputSelect.svelte
@@ -0,0 +1,29 @@
+<script lang="ts" context="module">
+	export interface Option {
+		label: string;
+		value: string;
+	}
+</script>
+
+<script lang="ts">
+	import styles from "./input-select.module.css";
+
+	export let disabled = false;
+	export let name: string;
+	export let options: Option[];
+	export let value: string;
+</script>
+
+<label class="{styles.wrapper}">
+	<slot />
+	<select
+		bind:value
+		class="{styles.select}"
+		{disabled}
+		{name}
+	>
+		{#each options as { value, label }}
+			<option {value}>{label}</option>
+		{/each}
+	</select>
+</label>
