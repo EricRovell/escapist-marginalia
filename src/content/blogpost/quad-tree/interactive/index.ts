ce2a9bae990f33dd0ccbac5f0211840b762c4d11
EricRovell <ericrovell@gmail.com>
2023-01-24 01:57:20 +0300

    feat(blogpost): quadtree query interactive

---
+++ src/content/blogpost/quad-tree/interactive/index.ts
@@ -0,0 +1,3 @@
+export { default as QuadTreeDepth } from "./quadtree-depth/QuadTreeDepth.svelte";
+export { default as QuadTreeQuery } from "./quadtree-query/QuadTreeQuery.svelte";
+export { default as QuadTreeViz } from "./quadtree-viz/QuadTreeViz.svelte";
---
+++ src/content/blogpost/quad-tree/interactive/quadtree-query/QuadTreeQuery.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+	import { onDestroy } from "svelte";
+	import { QuadTreeGrid } from "../../components";
+	import { Button, InputSelect, Range } from "@components";
+	import { QuadTree, Circle, Rectangle } from "@utils/quad-tree";
+	import { randFloat } from "@utils/random";
+	import { clamp } from "@utils/helpers";
+	import styles from "./quadtree-query.module.css";
+
+	let element: SVGElement;
+	let points = [];
+	let pointsQuery = [];
+	let quadtree = new QuadTree(4, new Rectangle(0, 0, 1000, 1000));
+	let bounds = [];
+
+	let rect = false;
+	let show = false;
+
+	let width = 300;
+	let height = 200;
+	let x = 0;
+	let y = 0;
+	let r = 150;
+
+	const handlePointerEnter = () => {
+		element && element.addEventListener("pointermove", handlePointerMove);
+		element && element.style.setProperty("cursor", "move");
+		show = true;
+	};
+
+	const handlePointerLeave = () => {
+		element && element.removeEventListener("pointermove", handlePointerMove);
+		element && element.style.removeProperty("cursor");
+		show = false;
+	};
+
+	const handlePointerMove = (e: PointerEvent) => {
+		// @ts-expect-error: it works
+		let point = DOMPoint.fromPoint(element);
+		point.x = e.clientX;
+		point.y = e.clientY;
+		// @ts-expect-error: it works
+		point = point.matrixTransform(element.getScreenCTM().inverse());
+
+		if (rect) {
+			x = clamp(point.x - width / 2, 0, 1000 - width);
+			y = clamp(point.y - height / 2, 0, 1000 - height);
+		} else {
+			x = clamp(point.x, 0, 1000 + 2 * r);
+			y = clamp(point.y, 0, 1000 + 2 * r);
+		}
+
+		const boundary = rect
+			? new Rectangle(x, y, width, height)
+			: new Circle(x, y, r);
+
+		pointsQuery = quadtree.query(boundary);
+	};
+
+	const handleRandomPlacement = () => {
+		points = [];
+		bounds = [];
+		quadtree.clear();
+		const items = [];
+
+		for (let i = 0; i < 100; i++) {
+			const point = {
+				x: randFloat(0, 1000),
+				y: randFloat(0, 1000)
+			};
+			items.push(point);
+			quadtree.insert(point);
+		}
+
+		points = items;
+		bounds = quadtree.getBoundaries();
+	};
+
+	handleRandomPlacement();
+
+	onDestroy(() => {
+		element && handlePointerLeave();
+	});
+</script>
+
+<section class="interactive wide">
+	<div class="{styles.wrapper}" class:show>
+		<QuadTreeGrid
+			bind:element="{element}"
+			{bounds}
+			height="{1000}"
+			{points}
+			{pointsQuery}
+			width="{1000}"
+			on:pointerleave="{handlePointerLeave}"
+			on:pointerenter="{handlePointerEnter}"
+		>
+			{#if show}
+				{#if rect}
+					<rect
+						class="{styles.boundary}"
+						x="{x}"
+						y="{y}"
+						{height}
+						{width}
+						stroke="var(--surface-1)"
+						stroke-width="5px"
+						stroke-opacity="0.5"
+						fill="rgb(0 0 0 / 0.15)"
+					/>
+				{:else}
+					<circle
+						class="{styles.boundary}"
+						cx="{x}"
+						cy="{y}"
+						{r}
+						stroke="var(--surface-1)"
+						stroke-width="5px"
+						stroke-opacity="0.5"
+						fill="rgb(0 0 0 / 0.15)"
+					/>
+				{/if}
+			{/if}
+		</QuadTreeGrid>
+		<p>
+			Point over the area
+		</p>
+	</div>
+	<form on:submit|preventDefault>
+		<Button on:click="{handleRandomPlacement}">
+			Random 100
+		</Button>
+		<InputSelect
+			options="{[
+				{ label: "Rectangle", value: "rect", selected: rect === true },
+				{ label: "Circle", value: "circle", selected: rect === false }
+			]}"
+			on:change="{e => {
+				rect = e.target.value === "rect";
+			}}"
+		>
+			Boundary
+		</InputSelect>
+		{#if rect}
+			<Range
+				bind:value="{width}"
+				min="{50}"
+				max="{500}"
+				output
+			>
+				Boundary width
+			</Range>
+			<Range
+				bind:value="{height}"
+				min="{50}"
+				max="{500}"
+				output
+			>
+				Boundary height
+			</Range>
+		{:else}
+			<Range
+				bind:value="{r}"
+				min="{50}"
+				max="{500}"
+				output
+			>
+				Boundary radius
+			</Range>
+		{/if}
+	</form>
+</section>
