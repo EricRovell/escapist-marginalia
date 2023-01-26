6527b3c05cd3b20acdac79cd08fb039858345ea4
EricRovell <ericrovell@gmail.com>
2023-01-26 15:19:25 +0300

    feat(blogpost): comparing objects interactive

--- src/content/blogpost/quad-tree/interactive/index.ts
+++ src/content/blogpost/quad-tree/interactive/index.ts
@@ -1,3 +1,4 @@
 export { default as QuadTreeDepth } from "./quadtree-depth/QuadTreeDepth.svelte";
 export { default as QuadTreeQuery } from "./quadtree-query/QuadTreeQuery.svelte";
 export { default as QuadTreeViz } from "./quadtree-viz/QuadTreeViz.svelte";
+export { default as ComparingObjects } from "./comparing-objects/ComparingObjects.svelte";
