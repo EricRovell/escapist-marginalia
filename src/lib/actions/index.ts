24064966a15fd1ec1b901e1e413e916c7257316a
EricRovell <ericrovell@gmail.com>
2023-02-19 02:42:07 +0300

    chore: update actions index

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,4 +1,5 @@
 export { current } from "./current";
+export { clickOutside } from "./click-outside";
 export { pannable } from "./pannable";
 export { portal } from "./portal";
 export { shortcut } from "./shortcut";
