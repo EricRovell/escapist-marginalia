99ee24b06770999c008a61fb710af6a168020d83
EricRovell <ericrovell@gmail.com>
2023-07-09 00:49:57 +0300

    refactor: use updated api to get the images

--- src/routes/[locale]/home/+page.ts
+++ src/routes/[locale]/home/+page.ts
@@ -1,6 +1,6 @@
 import { error } from "@sveltejs/kit";
 import { getBlogposts } from "@data/posts";
-import { getGalleryItems } from "@data/photos";
+import { getGalleryItems } from "@data/gallery";
 import { getProjects } from "@data/projects";
 import type { Locale } from "@types";
 import type { PageLoad } from "./$types";
@@ -9,15 +9,28 @@ export const load: PageLoad = async ({ params }) => {
 	const { locale = "en" } = params as { locale: Locale };
 
 	try {
-		const [ blogposts, photos, projects ] = await Promise.all([
-			getBlogposts({ lang: locale, draft: false }),
-			getGalleryItems(),
-			getProjects({ featured: true, lang: locale }, { limit: 4 })
+		const [ blogposts, gallery, projects ] = await Promise.all([
+			getBlogposts({
+				lang: locale,
+				draft: false
+			}),
+			getGalleryItems({
+				lang: locale,
+				draft: false
+			}, {
+				limit: 5
+			}),
+			getProjects({
+				featured: true,
+				lang: locale
+			}, {
+				limit: 4
+			})
 		]);
 
 		return {
 			blogposts: blogposts.slice(0, 4),
-			photos: photos.slice(0, 4),
+			photos: gallery.slice(0, 4),
 			projects: projects.slice(0, 4)
 		};
 	} catch (err) {
