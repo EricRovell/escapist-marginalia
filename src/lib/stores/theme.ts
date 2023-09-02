9b2949235a84eb0785dc11135a50d5289edb5f52
EricRovell <ericrovell@gmail.com>
2023-09-02 23:00:43 +0300

    feat: remember user theme choice

--- src/lib/stores/theme.ts
+++ src/lib/stores/theme.ts
@@ -2,7 +2,15 @@ import { writable } from "svelte/store";
 import { mediaObserver } from "svelte-media-observer";
 import type { MediaQueries } from "svelte-media-observer";
 
-export type Theme = "light" | "dark" | "auto";
+export type ThemeType = "light" | "dark";
+export type ThemeOptions = ThemeType | "system";
+
+export type Theme = {
+	type: "user";
+	value: "light" | "dark"
+} | {
+	type: "system"
+}
 
 const mediaQueries: MediaQueries<"light"> = [
 	[ "light", "(prefers-color-scheme: light)" ]
@@ -11,40 +19,64 @@ const mediaQueries: MediaQueries<"light"> = [
 export const media = mediaObserver(mediaQueries);
 
 function getThemePreference(): Theme {
-	if (globalThis.window) {
-		const theme = document.documentElement.getAttribute("theme");
-		if (theme === "dark" || theme === "light") {
-			return theme;
-		}
+	if (!globalThis.window) {
+		return {
+			type: "system"
+		};
 	}
 
-	return "auto";
+	const storedTheme = localStorage.getItem("theme") as ThemeType;
+
+	if (storedTheme) {
+		return {
+			type: "user",
+			value: storedTheme
+		};
+	}
+
+	const currentTheme = document.documentElement.getAttribute("theme") as ThemeType;
+
+	if (currentTheme) {
+		return {
+			type: "user",
+			value: storedTheme
+		};
+	}
+
+	return {
+		type: "system"
+	};
 }
 
 function initThemeStore() {
 	const initial: Theme = getThemePreference();
-	const { subscribe, update } = writable<Theme>(initial);
+	const { subscribe, set } = writable<Theme>(initial);
 
 	return {
 		subscribe,
-		change: () => update(value => {
-			if (value === "light") return "dark";
-			if (value === "dark") return "auto";
-			return "light";
-		})
+		set: (theme: ThemeOptions) => {
+			if (theme === "system") {
+				set({ type: "system" });
+			} else {
+				set({ type: "user", value: theme });
+			}
+		}
 	};
 }
 
 export const theme = initThemeStore();
 
-theme.subscribe(value => {
+theme.subscribe(theme => {
 	if (!globalThis.window) {
 		return;
 	}
 
-	if (value !== "auto") {
-		document.documentElement.setAttribute("theme", value);
-	} else {
+	if (theme.type === "system") {
+		localStorage.removeItem("theme");
 		document.documentElement.removeAttribute("theme");
+		return;
 	}
+
+	localStorage.setItem("theme", theme.value);
+	document.documentElement.setAttribute("theme", theme.value);
 });
