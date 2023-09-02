13fc45549c0bdad349490924897affd41ecf3195
EricRovell <ericrovell@gmail.com>
2023-09-02 22:46:14 +0300

    refactor: move footer to views

--- src/lib/views/footer/footer.locale.svelte
+++ src/lib/views/footer/footer.locale.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import { locale, t } from "@core/i18n";
-	import { Link } from "../link";
-	import styles from "./nav-locale.module.css";
+	import { Link } from "@components";
+	import styles from "./footer.module.css";
 
 	const regex = /^\/(ru|en)\//;
 
@@ -13,18 +13,18 @@
 </script>
 
 <nav
-	class={styles.nav}
-	title={$t("tooltip.change-locale")}
+	class="{styles["nav-locale"]}"
+	title="{$t("tooltip.change-locale")}"
 >
 	<Link
-		aria-current={$locale === "en" ? true : undefined}
-		href={`/en/${href}`}
+		aria-current="{$locale === "en" ? true : undefined}"
+		href="{`/en/${href}`}"
 	>
 		<span>EN</span>
 	</Link>
 	<Link
-		aria-current={$locale === "ru" ? true : undefined}
-		href={`/ru/${href}`}
+		aria-current="{$locale === "ru" ? true : undefined}"
+		href="{`/ru/${href}`}"
 	>
 		<span>RU</span>
 	</Link>
---
+++ src/lib/views/footer/footer.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { Datetime, Link, Icon } from "@components";
+	import NavLocale from "./footer.locale.svelte";
+	import { iconGithub, iconRSS } from "../../components/icons/default";
+	import { t } from "@core/i18n";
+	import styles from "./footer.module.css";
+
+	import {
+		pathSource,
+		pathLicense,
+		pathPrivacy,
+		pathWriting,
+		pathGallery,
+		pathSketch,
+		pathBookmarks,
+		pathRSS,
+		pathAbout
+	} from "@core/paths";
+
+	$: links = [
+		{ label: "writing", href: $pathWriting },
+		{ label: "gallery", href: $pathGallery },
+		{ label: "sketch", href: $pathSketch },
+		{ label: "bookmarks", href: $pathBookmarks },
+		{ label: "license", href: $pathLicense },
+		{ label: "privacy", href: pathPrivacy, disabled: true },
+		{ label: "about", href: $pathAbout }
+	];
+
+	$: icons = [
+		{ label: "source", href: pathSource, icon: iconGithub },
+		{ label: "rss", href: $pathRSS, icon: iconRSS }
+	];
+</script>
+
+<footer class="surface-2 {styles.footer}">
+	<div class="container {styles.wrapper}">
+		<nav class={styles.navigation}>
+			<ul>
+				{#each links as { label, href, disabled = false }}
+					<li>
+						<Link {href} {disabled}>
+							{$t(`sections.${label}`)}
+						</Link>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+		<nav class={styles.socials}>
+			<ul>
+				{#each icons as { label, href, icon }}
+					<li>
+						<Link block {href}>
+							<Icon path={icon} title={$t(`sections.${label}`)} />
+						</Link>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+		<section class={styles.user}>
+			<p>
+				<span>
+					© <Datetime options={{ year: "numeric" }} />, Eric/Rovell
+				</span>
+				<span>{$t("message.build-at")} <Datetime date={"__buildTime__"} /></span>
+			</p>
+			<div class={styles.locale}>
+				<span>{$t("dict.language")}:</span>
+				<NavLocale />
+			</div>
+		</section>
+	</div>
+</footer>
