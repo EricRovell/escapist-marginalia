b1babfce358fd5292941771332c5760cad92b215
EricRovell <ericrovell@gmail.com>
2023-09-02 22:47:45 +0300

    feat(component): implement dialog

---
+++ src/lib/components/dialog/dialog.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { afterUpdate, createEventDispatcher } from "svelte";
+	import { Button } from "../button";
+	import Icon from "../icons/Icon.svelte";
+	import { iconCancel } from "../icons/default";
+	import styles from "./dialog.module.css";
+	import { clickOutside, shortcut } from "../../actions";
+	import { preventPageScroll } from "$lib/utils/helpers";
+
+	export let className = "";
+	export let element: HTMLDialogElement | undefined = undefined;
+	export let open = false;
+
+	const dispatch = createEventDispatcher();
+
+	const handleOpen = () => {
+		open = true;
+		element.showModal();
+		dispatch("open");
+	};
+
+	const handleClose = () => {
+		open = false;
+		element.close();
+		dispatch("close");
+	};
+
+	afterUpdate(() => {
+		open
+			? handleOpen()
+			: handleClose();
+
+		preventPageScroll(open);
+		
+		if (open) {
+			document.documentElement.setAttribute("inert", "inert");
+		} else {
+			document.documentElement.removeAttribute("inert");
+		}
+	});
+</script>
+
+<dialog
+	bind:this={element}
+	class="{styles.dialog} {className}"
+	on:close
+	use:shortcut={{
+		active: open,
+		code: "Escape",
+		callback: handleClose
+	}}
+>
+	<div class={styles.wrapper}>
+		<aside
+			class={styles.modal}
+			use:clickOutside={{ active: open, callback: handleClose }}
+		>
+			<header>
+				<slot name="title" />
+				<Button on:click={handleClose} type="button" icon appearance="ghost">
+					<Icon path="{iconCancel}" />
+				</Button>
+			</header>
+			<div class={styles.content}>
+				<div>
+					<slot />
+				</div>
+			</div>
+		</aside>
+	</div>
+</dialog>
