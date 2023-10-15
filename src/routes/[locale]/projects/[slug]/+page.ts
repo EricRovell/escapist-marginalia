bac74af87f494718afa513bda0a806ec6f7a4c5a
EricRovell <ericrovell@gmail.com>
2023-10-16 00:54:53 +0300

    fix: retrieve project pages correctly

--- src/routes/[locale]/projects/[slug]/+page.ts
+++ src/routes/[locale]/projects/[slug]/+page.ts
@@ -1,31 +1,23 @@
 import { error } from "@sveltejs/kit";
-import { getProjects } from "@data/projects";
 import type { PageLoad } from "./$types";
-import type { Locale, Page } from "@types";
+import type { Page, Project } from "@types";
 
 export const load: PageLoad = async ({ params }) => {
 	try {
 		const { locale = "en", slug } = params;
-
-		const project = await getProjects({
-			lang: locale as Locale,
-			title: slug
-		}, { limit: 1 });
-
-		if (!project.length) {
-			throw error(404, "There is no such a project.");
-		}
-
-		const filepathName = `${project[0].title}/index.${locale}`;
-
-		let page: Page<typeof project[number]>;
+		const filepathName = `/${slug}/index.${locale}`;
 		const modules = import.meta.glob("/src/content/project/**/*.svx");
+		let page: Page<Project>;
 
 		for (const [ filepath, module ] of Object.entries(modules)) {
 			if (filepath.includes(filepathName)) {
 				page = await module() as typeof page;
 				break;
 			}
+		}
+
+		if (!page) {
+			throw error(404, "There is no such a project.");
 		}
 
 		return {
