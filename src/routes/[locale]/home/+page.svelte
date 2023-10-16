885a783e78952430a4189f1215a0557fef19cd5b
EricRovell <ericrovell@gmail.com>
2023-10-17 00:37:39 +0300

    feat: improve home mobile menu cards layout

--- src/routes/[locale]/home/+page.svelte
+++ src/routes/[locale]/home/+page.svelte
@@ -22,89 +22,93 @@
 		</p>
 	</svelte:fragment>
 	<div class={styles.layout}>
-		<section aria-label="recent-posts">
-			<div>
+		<div class="{styles.wrapper}">
+			<section aria-label="recent-posts">
 				<header>
-					<h2 class="heading">{$t("dict.recent-posts")}</h2>
 					<Link href={$pathWriting}>
-						&#x2192; {$t("dict.all-posts")}
+						<h2 class="heading">{$t("dict.recent-posts")}</h2>
 					</Link>
 				</header>
-				<ul class="grid-flexible" style="--item-size: 24ch">
-					{#each data.blogposts as { cover, dateCreated, description, keywords, title, slug }}
-						<li>
-							<Card
-								{cover}
-								date="{dateCreated}"
-								{description}
-								href={$pathBlogpost(slug)}
-								{keywords}
-								{title}
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</section>
-		<section aria-label="featured-projects">
-			<div>
+				<div class="{styles["content-wrapper"]}">
+					<ul class="grid-flexible" style="--item-size: 24ch">
+						{#each data.blogposts as { cover, dateCreated, description, keywords, title, slug }}
+							<li>
+								<Card
+									{cover}
+									date="{dateCreated}"
+									{description}
+									href={$pathBlogpost(slug)}
+									{keywords}
+									{title}
+								/>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</section>
+		</div>
+		<div class="{styles.wrapper}">
+			<section aria-label="featured-projects">
 				<header>
-					<h2 class="heading">{$t("dict.featured-projects")}</h2>
 					<Link href={$pathProjects}>
-						&#x2192; {$t("dict.all-projects")}
+						<h2 class="heading">{$t("dict.featured-projects")}</h2>
 					</Link>
 				</header>
-				<ul class="grid-flexible" style="--item-size: 25ch">
-					{#each data.projects as { cover, dateUpdated, description, keywords, title, slug }}
-						<li>
-							<Card
-								{cover}
-								date="{dateUpdated}"
-								{description}
-								href={slug}
-								{title}
-								{keywords}
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</section>
-		<section aria-label="featured-sketches">
-			<div>
+				<div class="{styles["content-wrapper"]}">
+					<ul class="grid-flexible" style="--item-size: 25ch">
+						{#each data.projects as { cover, dateUpdated, description, keywords, title, slug }}
+							<li>
+								<Card
+									{cover}
+									date="{dateUpdated}"
+									{description}
+									href={slug}
+									{title}
+									{keywords}
+								/>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</section>
+		</div>
+		<div class="{styles.wrapper}">
+			<section aria-label="featured-sketches">
 				<header>
-					<h2 class="heading">{$t("dict.recent-sketches")}</h2>
 					<Link href={$pathSketch}>
-						&#x2192; {$t("dict.all-sketches")}
+						<h2 class="heading">{$t("dict.recent-sketches")}</h2>
 					</Link>
 				</header>
-				<ul class="grid-flexible" style="--item-size: 25ch">
-					{#each data.sketches as { cover, dateUpdated, description, keywords, title, slug }}
-						<li>
-							<Card
-								{cover}
-								date="{dateUpdated}"
-								{description}
-								href={slug}
-								{title}
-								{keywords}
-							/>
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</section>
-		<section aria-label="recent-photos">
-			<div>
-				<header>
-					<h2 class="heading">{$t("dict.recent-photos")}</h2>
-					<Link href={$pathGallery}>
-						&#x2192; {$t("dict.entire-gallery")}
-					</Link>
-				</header>
-				<Gallery items={data.gallery} />
-			</div>
-		</section>
+				<div class="{styles["content-wrapper"]}">
+					<ul class="grid-flexible" style="--item-size: 25ch">
+						{#each data.sketches as { cover, dateUpdated, description, keywords, title, slug }}
+							<li>
+								<Card
+									{cover}
+									date="{dateUpdated}"
+									{description}
+									href={slug}
+									{title}
+									{keywords}
+								/>
+							</li>
+						{/each}
+					</ul>
+				</div>
+			</section>
+		</div>
+		<div class="{styles.wrapper}">
+			<section aria-label="recent-photos">
+				<div>
+					<header>
+						<Link href={$pathGallery}>
+							<h2 class="heading">{$t("dict.recent-photos")}</h2>
+						</Link>
+					</header>
+					<Gallery items={data.gallery} />
+				</div>
+			</section>
+		</div>
 		<aside class="{styles.aside}">
 			<Link href="#app">
 				<Icon path="{iconArrowTop}" />
