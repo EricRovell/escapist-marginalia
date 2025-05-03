79dc4b64682e32b8eb3b60864e2b9234a3591782
EricRovell <ericrovell@gmail.com>
2025-05-03 16:49:26 +0300

    feat(ui): input component, wip

---
+++ packages/ui/src/components/input/input.tsx
@@ -0,0 +1,31 @@
+import { type JSX, splitProps  } from "solid-js";
+import { classnames, setAttribute } from "utils";
+
+import type { Classes } from "../../types";
+
+import styles from "./input.module.css";
+
+export interface InputProps extends Omit<JSX.InputHTMLAttributes<HTMLInputElement>, "class"> {
+	classes?: Classes<"input" | "label" | "root">;
+	error?: boolean;
+	label?: string;
+}
+
+// TODO: input hint
+export function Input(allProps: InputProps) {
+	const [ props, rest ] = splitProps(allProps, [ "classes", "error", "label" ]);
+
+	return (
+		<label class={classnames(styles.root, props.classes?.root)}>
+			<span class={classnames(styles.label, props.classes?.label)}>
+				{props.label}
+			</span>
+			<input
+				class={classnames(styles.input, props.classes?.input)}
+				data-error={setAttribute(props.error)}
+				type="text"
+				{...rest}
+			/>
+		</label>
+	);
+}
