ef1641081f1b40224f82bcd9673f559dbdfcd3f5
EricRovell <ericrovell@gmail.com>
2025-05-25 17:33:16 +0300

    feat(ui): dialog component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,9 +1,8 @@
 export { ButtonScrollTop } from "./button-scroll-top/button-scroll-top";
 export { Button } from "./button/button";
-export { Canvas, type RenderArguments, type Renderer } from "./canvas";
+export { Dialog } from "./dialog/dialog";
 export { InputColor } from "./input-color";
 export { InputRange } from "./input-range";
 export { InputToggle } from "./input-toggle";
 export { Link } from "./link/link";
 export { LoaderBinary } from "./loader-binary/loader-binary";
-export { type CreateSketch, Sketch } from "./sketch";
